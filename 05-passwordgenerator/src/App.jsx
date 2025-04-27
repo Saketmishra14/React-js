0203f2bd12e6500ca69544633e1db3bc22e47f67
saket_14 <mishrasaket042@gmail.com>
2025-04-27 12:33:18 +0000

    update password generator

--- 05-passwordgenerator/src/App.jsx
+++ 05-passwordgenerator/src/App.jsx
@@ -26,6 +26,7 @@ function App() {
 
 
   }, [length, numberAllowed, charAllowed, setPassword])
+  useEffect(( )=>{passwordGenerator()},[length,numberAllowed,charAllowed,passwordGenerator])
 
   return (
     <>
@@ -34,10 +35,41 @@ function App() {
     <div className="flex shadow rounded-lg overflow-hidden mb-4 bg-white">
       <input type="text" 
       value={password}
-      className='outline-none w-full py-1 px-3'
+      className='outline-none w-full py-1 px-3 text-black'
       placeholder='password'
       readOnly/>
       <button className='outline-none text-white bg-green-700 px-3 py-0.5 shrink-0'>Copy</button>
+    </div>
+    <div className='flex text-sm gap-x-2 text-white'>
+      <div className='flex items-center gap-x-1'>
+        <input type="range" 
+        min={6}
+        max={80}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e) => {setLength(e.target.value)}}/>
+        <label >Length:{length}</label>
+      </div>
+
+      <div className='flex items-center gap-x-1 text-white'>
+        <input type="checkbox"
+        defaultChecked={numberAllowed}
+        id='numberInput'
+        onChange={()=>{
+          setNumberAllowed((prev)=>!prev);
+        }} />
+        <label htmlFor="numberInput">Number</label>
+      </div>
+
+      <div className='flex items-center gap-x-1 text-white'>
+        <input type="checkbox"
+        defaultChecked={charAllowed}
+        id='characterInput'
+        onChange={()=>{
+          setCharAllowed((prev)=>!prev);
+        }} />
+        <label htmlFor="characterInput">Character</label>
+      </div>
     </div>
 
     </div>
